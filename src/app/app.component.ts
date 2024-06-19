119c12ccb3c75026d4223f0f6ae937d4ae6ec6a1
roel <roelsax@gmail.com>
2024-06-19 19:38:18 +0200

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/models/wishitem';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ang-proj';
+  items : WishItem[] = [
+     new WishItem('Learn Angular'),
+     new WishItem('Get Coffee', true),
+     new WishItem('Find grass that cuts itself')
+  ];
+
+  newWishText  = '';
+
+  title = 'wishlist';
+
+  public addNewWish(): void {
+    console.log('click');
+    return;
+    //event.preventDefault();
+    console.log('test');
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = '';
+  }
+
+  toggleItem(item : WishItem) {
+    item.isComplete = !item.isComplete;
+    console.log(item);
+  }
 }
